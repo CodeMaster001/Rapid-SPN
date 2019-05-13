5ebf42dc2fab4660e070aaffb62d7d487fc3a7c9
Prajay <prajay@Prajays-MacBook-Air.local>
2019-05-13 19:47:39 -0400

    Updated breast_cancer

--- breast_cancer.py
+++ breast_cancer.py
@@ -8,8 +8,9 @@ Spatial tree demo for matrix data
 
 import numpy
 import sys
-
-from spatialtree import spatialtree
+import os
+from sklearn import preprocessing
+from spatialtree import SPNRPBuilder
 from spn.structure.Base import Context
 from spn.io.Graphics import plot_spn
 from spn.algorithms.Sampling import sample_instances
@@ -20,81 +21,114 @@ from sklearn.datasets import load_iris,load_digits,fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_breast_cancer
 from spn.algorithms.LearningWrappers import learn_parametric
+from spn.gpu.TensorFlow import optimize_tf
+from spn.structure.Base import Product, Sum, assign_ids, rebuild_scopes_bottom_up
 from sklearn.metrics import accuracy_score
 from numpy.random.mtrand import RandomState
-from spn.algorithms.TransformStructure import Prune,Compress
 from spn.algorithms.LearningWrappers import learn_parametric, learn_classifier
-
-
-from spn.structure.Base import Product, Sum, assign_ids, rebuild_scopes_bottom_up
-from spn.algorithms.LearningWrappers import learn_parametric, learn_classifier
+import urllib
 import tensorflow as tf
-from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.datasets import fetch_openml
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-mnist = load_breast_cancer()
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-credit=pd.read_csv("breast-cancer-wisconsin.data",delimiter=",") 
+from sklearn.model_selection import KFold
+from spn.gpu.TensorFlow import eval_tf
+import time;
+numpy.random.seed(42)
+
+
+def  score(i):
+	if i == 'g':
+		return 0;
+	else:
+		return 1;
+
+def one_hot(df,col):
+	df = pd.get_dummies([col])
+	df.drop()
+
+
+
+credit,target = fetch_openml(name='breast-cancer', version=1,return_X_y=True)
+credit = pd.DataFrame(data=credit)
 credit = credit.apply(LabelEncoder().fit_transform)
+theirs = list()
+ours = list()
 kf = KFold(n_splits=10,shuffle=True)
 print(credit.shape)
-theirs = list()
-ours = list();
+ours_time_list = list();
+theirs_time_list = list();
+
 for train_index, test_index in kf.split(credit):
-	X = credit.values[train_index]
-	X_test = credit.values[test_index];
-	N = X.shape[0]
-	D = X.shape[1]
-
-	# First, create a random data matrix
-
-
-
-	# Apply a random projection so the data's not totally boring
-
-	#X = numpy.dot(X, P)
-
-	# Construct a tree.  By default, we get a KD-spill-tree with height
-	# determined automatically, and spill = 25%
-	context = [Categorical]*(D-1);
-	context.append(Categorical)
-
-	ds_context = Context(parametric_types=context).add_domains(X)
+	X = credit.values[train_index,:]
+	#y_train=target[train_index]
+	#X = preprocessing.normalize(X, norm='l2')
+	X_test = credit.values[test_index];	
+	#X_test = preprocessing.normalize(X_test, norm='l2')
+	y_test=target[test_index]
 
 
-	spn_classification = learn_parametric(X,ds_context,min_instances_slice=2)
-
-
-	ll_test_original= log_likelihood(spn_classification, X_test)
-
-	ll_test = log_likelihood(spn_classification, X_test)
-
+	context = list()
+	for i in range(0,X.shape[1]):
+		context.append(Categorical)
 
 
 
 
 
 	ds_context = Context(parametric_types=context).add_domains(X)
+	print("training normnal spm")
+	
+	theirs_time = time.time()
+	spn_classification =  learn_parametric(numpy.array(X),ds_context,threshold=0.3,min_instances_slice=2)
+	
+	
+	
+	theirs_time = time.time()-theirs_time
+
+	ll_original = log_likelihood(spn_classification, X)
+	ll = log_likelihood(spn_classification, X)
+	ll_test = log_likelihood(spn_classification,X_test)
+	ll_test_original=ll_test[ll_test>-1000]
+
+
+
+
 	print('Building tree...')
-	T = spatialtree(data=X,ds_context=ds_context,leaves_size=20,target=X.shape[1]-1,rule='rp',prob=0.2,height=3,spill=0.76)
+	original = time.time();
+	T = SPNRPBuilder(data=X,ds_context=ds_context,leaves_size=20,target=X.shape[1]-1,rule='rp',prob=0.2,height=2,spill=0.76)
 	print("Building tree complete")
-	T.update_ids()
-	spn = T.spn_node_object()
-
-
-	ll_test= log_likelihood(spn, X_test)
+	
+	T= T.build_spn();
+	T.update_ids();
+	from spn.io.Text import spn_to_str_equation
+	spn = T.spn_node;
+	ours_time = time.time()-original;
+	ours_time_list.append(ours_time)
+	ll = log_likelihood(spn, X)
+	ll_test = log_likelihood(spn,X_test)
 	ll_test=ll_test[ll_test>-1000]
-	ll_test_original =ll_test_original[ll_test_original>-1000]
 	print(numpy.mean(ll_test_original))
 	print(numpy.mean(ll_test))
-	
 	theirs.extend(ll_test_original)
 	ours.extend(ll_test)
-print(numpy.mean(theirs))
+	theirs_time_list.append(theirs_time)
+
+plot_spn(spn_classification, 'basicspn-original.png')
+plot_spn(spn, 'basicspn.png')
+print(theirs)
+print(ours)
+print(original)
+print('---Time---')
+print(numpy.mean(ours_time_list))
+print(numpy.mean(theirs_time_list))
+print('---ll---')
 print(numpy.mean(ours))
+print(numpy.mean(theirs))
 
 
 
 
-#print(numpy.mean(ll_test))
+
+
