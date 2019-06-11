6b024eab3bdf2d71bf329f9b5b88d0bec71c9aed
Prajay <prajay@Prajays-MacBook-Air.local>
2019-06-11 01:13:31 -0400

    updated krvsk and krvskp

--- krvsk.py
+++ krvsk.py
@@ -251,15 +251,16 @@ for train_index, test_index in kf.split(credit):
 
 plot_spn(spn_classification, 'basicspn-original.png')
 plot_spn(spn, 'basicspn.png')
-print(theirs)
-print(ours)
-print(original)
-print('---Time---')
-print(numpy.mean(ours_time_list))
 print(numpy.mean(theirs_time_list))
+print(numpy.var(theirs_time_list))
+print(numpy.mean(ours_time_list))
+print(numpy.var(ours_time_list))
 print('---ll---')
-print(numpy.mean(ours))
 print(numpy.mean(theirs))
+print(numpy.var(theirs))
+
+print(numpy.mean(ours))
+print(numpy.var(ours))
 
 
 
