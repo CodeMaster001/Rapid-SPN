a68d49b1832687d9203679a559366690809565d1
Prajay <prajay@Prajays-MacBook-Air.local>
2019-07-11 21:40:38 -0400

    updated wine

--- scripts/wine_quality.py
+++ scripts/wine_quality.py
@@ -9,7 +9,7 @@ Spatial tree demo for matrix data
 import numpy
 import sys
 import os
-
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import random
 from sklearn import preprocessing
 from spatialtree import SPNRPBuilder
