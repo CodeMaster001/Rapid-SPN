4afd42ddce6ec9c492a2f668141609e1ba55cf82
prajay shetty <prajay.shetty24@gmail.com>
2020-01-17 15:28:00 -0500

    Updatd balance scale

--- scripts/balance_scale.py
+++ scripts/balance_scale.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+'''
+CREATED:2011-11-12 08:23:33 by Brian McFee <bmcfee@cs.ucsd.edu>
+
+Spatial tree demo for matrix data
+'''
+
 
 import numpy
 import sys
@@ -58,7 +64,8 @@ def bfs(root, func):
 
 def print_prob(node):
     if isinstance(node,Sum):
-        node.weights= np.random.dirichlet(np.ones(len(node.weights)),size=1)[0]
+        if node.weights[0]==node.weights[1]:
+            node.weights= np.random.dirichlet(np.ones(len(node.weights)),size=1)[0]
 def  score(i):
     if i == 'g':
         return 0;
@@ -76,7 +83,68 @@ def clean_data(x):
     except:
         print(str(x))
 
+
+def libsvm_preprocess(file_name,row_size=62,colon_size=200):
+    dataset_label, dataset_feature = svm_read_problem('../dataset/colon.csv')
+    X=list()
+    Y=list()
+    for i in range(0,row_size):
+        temp = list();
+        for j in range(0,colon_size):
+            temp.append(dataset_feature[i][j+1])
+        X.append(temp)
+        Y.append(dataset_label[i])
+    return np.array(X),np.array(Y)
  
+
+#print(credit.head())
+
+def spnrp_train(X,X_test):
+    context = list()
+    for i in range(0,X.shape[1]):
+        context.append(Gaussian)
+
+
+
+
+    ds_context = Context(parametric_types=context).add_domains(X)
+    original = time.time();
+    T = SPNRPBuilder(data=numpy.array(X),ds_context=ds_context,target=X,prob=0.5,leaves_size=3,height=3,spill=0.3)
+    print("Buiding tree complete")
+
+    T= T.build_spn();
+    T.update_ids();
+    ours_time = time.time()-original;
+    spn = T.spn_node;
+    #spn=optimize_tf(spn,X,epochs=1000)
+    #ll_test = eval_tf(spn,X_test)
+    tf.reset_default_graph()
+    del spn;
+    return np.mean(ll_test),ours_time
+
+def learnspn_train(X,X_test):
+    context = list()
+    for i in range(0,X.shape[1]):
+        context.append(Gaussian)
+
+
+
+
+    ds_context = Context(parametric_types=context).add_domains(X)
+    theirs_time = time.time()
+    spn_classification =  learn_parametric(numpy.array(X),ds_context,min_instances_slice=30)
+    theirs_time = time.time()-theirs_time
+    #spn_classification = optimize_tf(spn_classification,X,epochs=1000,optimizer= tf.train.AdamOptimizer(0.0001)) 
+        #tf.train.AdamOptimizer(1e-4))
+
+
+    #ll_test = eval_tf(spn_classification,X_test)
+    #print(ll_test)
+    ll_test = log_likelihood(spn_classifiation,X_test)
+    ll_test_original=ll_test
+    tf.reet_default_graph()
+    del spn_classification
+    return  np.mean(ll_test),theirs_time
 
 # experiment.py train.csv test.csv context.npy instance_slice epochs height prob leaves_size
 train_dataset,labels= fetch_openml(name='balance-scale', version=1,return_X_y=True)
@@ -93,37 +161,43 @@ counter = 0;
 context = list()
 
 #parameters
-output_file_name='balance.log'
-min_instances_slice=20
 epochs=8000
-height=4
-prob=0.6
-leaves_size=2
-threshold=0.3
+height=5
+prob=0.4
+leaves_size=20
+threshold =0.4
 
-opt_args= str(output_file_name) + ' ' + str(min_instances_slice) +' ' +str(epochs) + ' '+ str(height) + ' '+str(prob) + ' ' +str(leaves_size)+' '+str(threshold)
 
 for i in range(0,train_dataset_df.shape[1]):
     context.append(Gaussian)
-for train_index,test_index in kf.split(train_dataset_df):
-    X_train,X_test=train_dataset_df.values[train_index],train_dataset_df.values[test_index]
-    X=numpy.nan_to_num(X_train)
-    X = X.astype(numpy.float32)
-    X = preprocessing.normalize(X, norm='l2') 
-    X_test = numpy.nan_to_num(X_test)
-    X_test = preprocessing.normalize(X_test, norm='l2')
-    X = X.astype(numpy.float32)
-    X_test =X_test.astype(numpy.float32)
-    train_set.append(X)
-    test_set.append(X_test)
-    np.savetxt('train.csv', X, delimiter=',')
-    np.savetxt("test.csv",X_test,delimiter=',')
-    np.save("context",context)
-    P=subprocess.Popen(['./experiment.py train.csv test.csv context.npy '+opt_args.strip()],shell=True)
-    P.communicate()
-    P.wait();
-    P.terminate()
-print("process completed")
+for j in [15]:
+    output_file_name='balance_'+str(j)+'.log'
+    min_instances_slice=j
+    opt_args= str(output_file_name) + ' ' + str(min_instances_slice) +' ' +str(epochs) + ' '+ str(height) + ' '+str(prob) + ' ' +str(leaves_size)+' ' + str(threshold)
+    for train_index,test_index in kf.split(train_dataset_df):
+        X_train,X_test=train_dataset_df.values[train_index],train_dataset_df.values[test_index]
+        X=numpy.nan_to_num(X_train)
+        X = X.astype(numpy.float32)
+        X = preprocessing.normalize(X, norm='l2') 
+        X_test = numpy.nan_to_num(X_test)
+        X_test = preprocessing.normalize(X_test, norm='l2')
+        X = X.astype(numpy.float32)
+        X_test =X_test.astype(numpy.float32)
+        train_set.append(X)
+        test_set.append(X_test)
+        np.savetxt('train.csv', X, delimiter=',')
+        np.savetxt("test.csv",X_test,delimiter=',')
+        np.save("context",context)
+        P=subprocess.Popen(['./experiment.py train.csv test.csv context.npy '+opt_args.strip()],shell=True)
+        P.communicate()
+        P.wait();
+        P.terminate()
+    print("process completed")
 #!/usr/bin/env python
+'''
+CREATED:2011-11-12 08:23:33 by Brian McFee <bmcfee@cs.ucsd.edu>
+
+Spatial tree demo for matrix data
+'''
 
 
