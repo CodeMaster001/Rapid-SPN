045437c5ff75064898a1133d550eea32cb390dad
prajay shetty <prajay.shetty24@gmail.com>
2020-01-23 17:19:12 -0500

    updated

--- scripts/cars.py
+++ scripts/cars.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 
 import numpy
 import sys
@@ -42,51 +40,64 @@ numpy.random.seed(42)
 import multiprocessing
 import logging
 import subprocess
-
-train_dataset,labels= fetch_openml(name='cars', version=1,return_X_y=True)
-train_dataset_df = pd.DataFrame(train_dataset)
-
-kf = KFold(n_splits=10,shuffle=True)
-theirs = list()
-ours = list()
-ours_time_list = list()
-theirs_time_list = list();
-train_set = list()
-test_set = list();
-counter = 0;
-context = list()
-
-#parameters
-output_file_name='cars.log'
-min_instances_slice=100
-epochs=8000
-height=3
-prob=0.4
-leaves_size=15
-threshold=0.4
-opt_args= str(output_file_name) + ' ' + str(min_instances_slice) +' ' +str(epochs) + ' '+ str(height) + ' '+str(prob) + ' ' +str(leaves_size)+' '+str(threshold)
+#tf.logging.set_verbosity(tf.logging.INFO)
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
 
-for i in range(0,train_dataset_df.shape[1]):
-    context.append(Gaussian)
-for train_index,test_index in kf.split(train_dataset_df):
-    X_train,X_test=train_dataset_df.values[train_index],train_dataset_df.values[test_index]
-    X=numpy.nan_to_num(X_train)
-    X = X.astype(numpy.float32)
-    X = preprocessing.normalize(X, norm='l2') 
-    X_test = numpy.nan_to_num(X_test)
-    X_test = preprocessing.normalize(X_test, norm='l2')
-    X = X.astype(numpy.float32)
-    X_test =X_test.astype(numpy.float32)
-    train_set.append(X)
-    test_set.append(X_test)
-    np.savetxt('train.csv', X, delimiter=',')
-    np.savetxt("test.csv",X_test,delimiter=',')
-    np.save("context",context)
-    P=subprocess.Popen(['./experiment.py train.csv test.csv context.npy '+opt_args.strip()],shell=True)
-    P.communicate()
-    P.wait();
-    P.terminate()
-print("process completed")
+
+# experiment.py train.csv test.csv context.npy instance_slice epochs height prob leaves_size
+for instance in [10,15,20,25,30,35,40,45,50]:
+    train_dataset,labels= fetch_openml(name='cars', version=1,return_X_y=True)
+    train_dataset_df = pd.DataFrame(train_dataset)
+
+    kf = KFold(n_splits=int(sys.argv[1]),shuffle=True)
+    theirs = list()
+    ours = list()
+    ours_time_list = list()
+    theirs_time_list = list();
+    train_set = list()
+    test_set = list();
+    counter = 0;
+    context = list()
+
+    #parameters
+    output_file_name='cars.'+str(instance)+'.'+str(sys.argv[1])+'.log'
+    min_instances_slice=instance
+    epochs=8000
+    height=1
+    prob=0.4
+    leaves_size=15
+    threshold =0.4
+
+    opt_args= str(output_file_name) + ' ' + str(min_instances_slice) +' ' +str(epochs) + ' '+ str(height) + ' '+str(prob) + ' ' +str(leaves_size)+' ' + str(threshold)
+
+    for i in range(0,train_dataset_df.shape[1]):
+        context.append(Gaussian)
+    for train_index,test_index in kf.split(train_dataset_df):
+        X_train,X_test=train_dataset_df.values[train_index],train_dataset_df.values[test_index]
+        X=numpy.nan_to_num(X_train)
+        X = X.astype(numpy.float32)
+        X = preprocessing.normalize(X, norm='l2') 
+        X_test = numpy.nan_to_num(X_test)
+        X_test = preprocessing.normalize(X_test, norm='l2')
+        X = X.astype(numpy.float32)
+        X_test =X_test.astype(numpy.float32)
+        train_set.append(X)
+        test_set.append(X_test)
+        np.save('train', X)
+        np.save("test",X_test)
+        np.save("context",context)
+        P=subprocess.Popen(['./experiment.py train.npy test.npy context.npy '+opt_args.strip()],shell=True)
+        P.communicate()
+        P.wait();
+        P.terminate()
+    print("process completed")
+#!/usr/bin/env python
+'''
+CREATED:2011-11-12 08:23:33 by Brian McFee <bmcfee@cs.ucsd.edu>
+
+Spatial tree demo for matrix data
+'''
 
 
