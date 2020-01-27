f3d44ede4e0f3445f883bde73dc354c3da897603
prajay shetty <prajay.shetty24@gmail.com>
2020-01-26 21:45:15 -0500

    Iris 10 and 40

--- scripts/iris_40.py
+++ scripts/iris_40.py
@@ -51,147 +51,57 @@ import subprocess
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
-def bfs(root, func):
-    seen, queue = set([root]), collections.deque([root])
-    while queue:
-        node = queue.popleft()
-        func(node)
-        if not isinstance(node, Leaf):
-            for c in node.children:
-                if c not in seen:
-                    seen.add(c)
-                    queue.append(c)
 
-def print_prob(node):
-    if isinstance(node,Sum):
-        if node.weights[0]==node.weights[1]:
-            node.weights= np.random.dirichlet(np.ones(len(node.weights)),size=1)[0]
-def  score(i):
-    if i == 'g':
-        return 0;
-    else:
-        return 1;
-
-def one_hot(df,col):
-    df = pd.get_dummies([col])
-    df.drop()
-
-
-def clean_data(x):
-    try:
-        return str(x).split(':')[-1]
-    except:
-        print(str(x))
-
-
-def libsvm_preprocess(file_name,row_size=62,colon_size=200):
-    dataset_label, dataset_feature = svm_read_problem('../dataset/colon.csv')
-    X=list()
-    Y=list()
-    for i in range(0,row_size):
-        temp = list();
-        for j in range(0,colon_size):
-            temp.append(dataset_feature[i][j+1])
-        X.append(temp)
-        Y.append(dataset_label[i])
-    return np.array(X),np.array(Y)
- 
-
-#print(credit.head())
-
-def spnrp_train(X,X_test):
-    context = list()
-    for i in range(0,X.shape[1]):
-        context.append(Gaussian)
-
-
-
-
-    ds_context = Context(parametric_types=context).add_domains(X)
-    original = time.time();
-    T = SPNRPBuilder(data=numpy.array(X),ds_context=ds_context,target=X,prob=0.5,leaves_size=3,height=3,spill=0.3)
-    print("Buiding tree complete")
-
-    T= T.build_spn();
-    T.update_ids();
-    ours_time = time.time()-original;
-    spn = T.spn_node;
-    spn=optimize_tf(spn,X,epochs=1000)
-    ll_test = eval_tf(spn,X_test)
-    tf.reset_default_graph()
-    del spn;
-    return np.mean(ll_test),ours_time
-
-def learnspn_train(X,X_test):
-    context = list()
-    for i in range(0,X.shape[1]):
-        context.append(Gaussian)
-
-
-
-
-    ds_context = Context(parametric_types=context).add_domains(X)
-    theirs_time = time.time()
-    spn_classification =  learn_parametric(numpy.array(X),ds_context,min_instances_slice=30)
-    theirs_time = time.time()-theirs_time
-    spn_classification = optimize_tf(spn_classification,X,epochs=1000,optimizer= tf.train.AdamOptimizer(0.0001)) 
-        #tf.train.AdamOptimizer(1e-4))
-
-
-    ll_test = eval_tf(spn_classification,X_test)
-    #print(ll_test)
-    #ll_test = log_likelihood(spn_classification,X_test)
-    ll_test_original=ll_test
-    tf.reset_default_graph()
-    del spn_classification
-    return  np.mean(ll_test),theirs_time
 
 # experiment.py train.csv test.csv context.npy instance_slice epochs height prob leaves_size
-train_dataset,labels= fetch_openml(name='iris', version=1,return_X_y=True)
-train_dataset_df = pd.DataFrame(train_dataset)
+for instance in [40]:
+    train_dataset,labels= fetch_openml(name='iris', version=1,return_X_y=True)
+    train_dataset_df = pd.DataFrame(train_dataset)
 
-kf = KFold(n_splits=40,shuffle=True)
-theirs = list()
-ours = list()
-ours_time_list = list()
-theirs_time_list = list();
-train_set = list()
-test_set = list();
-counter = 0;
-context = list()
+    kf = KFold(n_splits=int(sys.argv[1]),shuffle=True)
+    theirs = list()
+    ours = list()
+    ours_time_list = list()
+    theirs_time_list = list();
+    train_set = list()
+    test_set = list();
+    counter = 0;
+    context = list()
 
-#parameters
-output_file_name='iris_40.log'
-min_instances_slice=20
-epochs=8000
-height=4
-prob=0.4
-leaves_size=15
-threshold =0.4
+    #parameters
+    output_file_name='iris.'+str(instance)+'.'+str(sys.argv[1])+'.log'
+    min_instances_slice=instance
+    epochs=8000
+    height=8
+    prob=0.4
+    leaves_size=15
+    threshold =0.4
+    selector_array=[2,3]
+    np.save('selector',np.array(selector_array))
 
-opt_args= str(output_file_name) + ' ' + str(min_instances_slice) +' ' +str(epochs) + ' '+ str(height) + ' '+str(prob) + ' ' +str(leaves_size)+' ' + str(threshold)
+    opt_args= str(output_file_name) + ' ' + str(min_instances_slice) +' ' +str(epochs) + ' '+ str(height) + ' '+str(prob) + ' ' +str(leaves_size)+' ' + str(threshold)
 
-for i in range(0,train_dataset_df.shape[1]):
-    context.append(Gaussian)
-for train_index,test_index in kf.split(train_dataset_df):
-    X_train,X_test=train_dataset_df.values[train_index],train_dataset_df.values[test_index]
-    X=numpy.nan_to_num(X_train)
-    X = X.astype(numpy.float32)
-    X = preprocessing.normalize(X, norm='l2') 
-    X_test = numpy.nan_to_num(X_test)
-    X_test = preprocessing.normalize(X_test, norm='l2')
-    X = X.astype(numpy.float32)
-    X_test =X_test.astype(numpy.float32)
-    train_set.append(X)
-    test_set.append(X_test)
-    np.savetxt('train.csv', X, delimiter=',')
-    np.savetxt("test.csv",X_test,delimiter=',')
-    np.save("context",context)
-    P=subprocess.Popen(['./experiment.py train.csv test.csv context.npy '+opt_args.strip()],shell=True)
-    P.communicate()
-    P.wait();
-    P.terminate()
-print("process completed")
+    for i in range(0,train_dataset_df.shape[1]):
+        context.append(Gaussian)
+    for train_index,test_index in kf.split(train_dataset_df):
+        X_train,X_test=train_dataset_df.values[train_index],train_dataset_df.values[test_index]
+        X=numpy.nan_to_num(X_train)
+        X = X.astype(numpy.float32)
+        X = preprocessing.normalize(X, norm='l2') 
+        X_test = numpy.nan_to_num(X_test)
+        X_test = preprocessing.normalize(X_test, norm='l2')
+        X = X.astype(numpy.float32)
+        X_test =X_test.astype(numpy.float32)
+        train_set.append(X)
+        test_set.append(X_test)
+        np.save('train', X)
+        np.save("test",X_test)
+        np.save("context",context)
+        P=subprocess.Popen(['./experiment.py train.npy test.npy context.npy '+opt_args.strip()],shell=True)
+        P.communicate()
+        P.wait();
+        P.terminate()
+    print("process completed")
 #!/usr/bin/env python
 '''
 CREATED:2011-11-12 08:23:33 by Brian McFee <bmcfee@cs.ucsd.edu>
