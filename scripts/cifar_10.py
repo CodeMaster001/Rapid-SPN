649782e2934bf312337af546f815be8af8f21d0b
Prajay Shetty <cody@Prajays-MacBook-Air.local>
2020-03-30 07:29:10 -0400

    Added  main cifar-10 file for running repeated results from sampels based on parameters recived from grid search

--- scripts/cifar_10.py
+++ scripts/cifar_10.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python
+'''
+CREATED:2011-11-12 08:23:33 by Brian McFee <bmcfee@cs.ucsd.edu>
+
+Spatial tree demo for matrix data
+'''
 
 
 import numpy
 import sys
 import os
+os.environ['NUMEXPR_MAX_THREADS'] = '16'
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from sklearn import preprocessing
 from sklearn.datasets import load_svmlight_file
@@ -57,85 +64,62 @@ def bfs(root, func):
                     seen.add(c)
                     queue.append(c)
 
-def print_prob(node):
-    if isinstance(node,Sum):
-        node.weights= np.random.dirichlet(np.ones(len(node.weights)),size=1)[0]
-def  score(i):
-    if i == 'g':
-        return 0;
-    else:
-        return 1;
-
-def one_hot(df,col):
-    df = pd.get_dummies([col])
-    df.drop()
 
 
-def clean_data(x):
-    try:
-        return str(x).split(':')[-1]
-    except:
-        print(str(x))
+print('Command Line Parameters:rows columns ratio repeat instance_slice threshold height leaves_size')
 
+rows_size=int(sys.argv[1])
+columns_size=int(sys.argv[2])
+ratio = float(sys.argv[3])
+repeat=int(sys.argv[4])
+instance_slice=int(sys.argv[5])
+threshold=float(sys.argv[6])
+height=int(sys.argv[7])
+leaves_size=int(sys.argv[8])
+print(sys.argv)
  # experiment.py train.csv test.csv context.npy instance_slice epochs height prob leaves_size
 train_dataset,labels= fetch_openml(name='CIFAR_10', version=1,return_X_y=True)
-train_dataset = pd.DataFrame(train_dataset)
+train_dataset_pd = pd.DataFrame(train_dataset).copy();
 
-train_dataset=train_dataset.sample(n=int(sys.argv[1])).values
-train_dataset=train_dataset[:,:int(sys.argv[2])]
-X_train,X_test=train_test_split(train_dataset,test_size=0.3)
+for i in range(0,repeat):
 
-# experiment.py train.csv test.csv context.npy instance_slice epochs height prob leaves_sizev
+    print(train_dataset_pd.values.shape)
 
-kf = KFold(n_splits=10,shuffle=True)
-theirs = list()
-ours = list()
-ours_time_list = list()
-theirs_time_list = list();
-train_set = list()
-test_set = list();
-counter = 0;
-context = list()
+    train_dataset=np.array(train_dataset_pd.sample(n=int(rows_size)).values)
+    train_dataset=train_dataset[:,:int(columns_size)]
+    X_train,X_test=train_test_split(train_dataset,test_size=ratio)
+    print(X_train.shape)
+    print(X_test.shape)
+    # experiment.py train.csv test.csv context.npy instance_slice epochs height prob leaves_sizev
 
+    output_file_name='mnist_spnrp_'+str(columns_size)+'.log'
+    epochs=8000
+    selector_array=[2,3,4]
+    np.save('selector',np.array(selector_array))
+    context = list()
 
-output_file_name='cifar_'+str(sys.argv[2])+'.log'
-epochs=8000
-height=1
-prob=0.5
-leaves_size=30
-threshold=0.4
-bandwidth=0.2
-predict_bandwidth=1
-selector_array=[2,3,4]
-np.save('selector',np.array(selector_array))
+    for j in range(0,train_dataset.shape[1]):
+        context.append(Gaussian)
 
-for i in range(0,train_dataset.shape[1]):
-    context.append(Gaussian)
-
-X=numpy.nan_to_num(X_train)
-X = X.astype(numpy.float32)
-X = preprocessing.normalize(X, norm='l2') 
-X_test = numpy.nan_to_num(X_test)
-X_test = preprocessing.normalize(X_test, norm='l2')
-X = X.astype(numpy.float32)
-X_test =X_test.astype(numpy.float32)
-train_set.append(X)
-
-test_set.append(X_test)
-print("--")
-print(X.shape)
-print(X_test.shape)
-np.save('train', X)
-np.save("test",X_test)
-np.save("context",context)
-for instance_slice in [10 ,20,40,60,100,200,300,400,500,1000,1500,2000]:
+    X=numpy.nan_to_num(X_train)
+    X = X.astype(numpy.float32)
+    X = preprocessing.normalize(X, norm='l2') 
+    X_test = numpy.nan_to_num(X_test)
+    X_test = preprocessing.normalize(X_test, norm='l2')
+    X = X.astype(numpy.float32)
+    X_test =X_test.astype(numpy.float32)
+    print("--")
+    print(X.shape)
+    print(X_test.shape)
+    np.save('train', X)
+    np.save("test",X_test)
+    np.save("context",context)
     opt_args= str(output_file_name) + ' ' + str(instance_slice) +' ' +str(height) + ' '+str(leaves_size)+' '+str(threshold) 
     P=subprocess.Popen(['./experiment.py train.npy test.npy context.npy '+opt_args.strip()],shell=True)
     P.communicate()
     P.wait();
     P.terminate()
     print("process completed")
-    break;
 
 #!/usr/bin/env python
 '''
@@ -143,4 +127,5 @@ CREATED:2011-11-12 08:23:33 by Brian McFee <bmcfee@cs.ucsd.edu>
 
 Spatial tree demo for matrix data
 '''
+
 
