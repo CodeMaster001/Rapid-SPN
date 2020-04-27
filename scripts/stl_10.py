c2e51e14a07ff444a985567ee3405acb847abd59
Prajay Shetty <cody@Prajays-MacBook-Air.local>
2020-04-26 20:47:58 -0400

    Updated stl 10 dataset

--- scripts/stl_10.py
+++ scripts/stl_10.py
@@ -3,7 +3,6 @@
 import numpy
 import sys
 import os
-os.environ['NUMEXPR_MAX_THREADS'] = '16'
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from sklearn import preprocessing
 from sklearn.datasets import load_svmlight_file
