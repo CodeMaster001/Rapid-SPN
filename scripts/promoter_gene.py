d020d4fe83223329f6dcb500573a33c1f1a25b9b
Prajay Shetty <prajay.shetty24@gmail.com>
2020-07-30 18:12:36 +0530

    Updated promoter gene

--- scripts/promoter_gene.py
+++ scripts/promoter_gene.py
@@ -47,52 +47,52 @@ logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
 
 # experiment.py train.csv test.csv context.npy instance_slice epochs height prob leaves_size
-for instance in [5,10,15,20,25,30,35,40,45,50]:
-    train_dataset,labels= fetch_openml(name='molecular-biology_promoters', version=1,return_X_y=True)
-    train_dataset_df = pd.DataFrame(train_dataset)
 
-    kf = KFold(n_splits=int(sys.argv[1]),shuffle=True)
-    theirs = list()
-    ours = list()
-    ours_time_list = list()
-    theirs_time_list = list();
-    train_set = list()
-    test_set = list();
-    counter = 0;
-    context = list()
+train_dataset,labels= fetch_openml(name='molecular-biology_promoters', version=1,return_X_y=True)
+train_dataset_df = pd.DataFrame(train_dataset)
 
-    #parameters
-    output_file_name='promoter.'+str(instance)+'.'+str(sys.argv[1])+'.log'
-    min_instance_slice=instance
-    epochs=8000
-    height=2
-    prob=0.4
-    leaves_size=15
-    threshold =0.4
+kf = KFold(n_splits=10,shuffle=True)
+theirs = list()
+ours = list()
+ours_time_list = list()
+theirs_time_list = list();
+train_set = list()
+test_set = list();
+counter = 0;
+context = list()
 
-    opt_args= str(output_file_name) + ' ' + str(min_instance_slice) + ' ' + str(height) +' '+ str(leaves_size) + ' ' +str(threshold)
+#parameters
+output_file_name='promoter.10.log'
+min_instance_slice=50
+epochs=8000
+height=2
+prob=0.4
+leaves_size=15
+threshold =0.4
 
-    for i in range(0,train_dataset_df.shape[1]):
-        context.append(Categorical)
-    for train_index,test_index in kf.split(train_dataset_df):
-        X_train,X_test=train_dataset_df.values[train_index],train_dataset_df.values[test_index]
-        X=numpy.nan_to_num(X_train)
-        X = X.astype(numpy.float32)
-        #X = preprocessing.normalize(X, norm='l2')
-        X_test = numpy.nan_to_num(X_test)
-        #X_test = preprocessing.normalize(X_test, norm='l2')
-        X = X.astype(numpy.float32)
-        X_test =X_test.astype(numpy.float32)
-        train_set.append(X)
-        test_set.append(X_test)
-        np.save('train', X)
-        np.save("test",X_test)
-        np.save("context",context)
-        P=subprocess.Popen(['./experiment.py train.npy test.npy context.npy '+opt_args.strip()],shell=True)
-        P.communicate()
-        P.wait();
-        P.terminate()
-    print("process completed")
+opt_args= str(output_file_name) + ' ' + str(min_instance_slice) + ' ' + str(height) +' '+ str(leaves_size) + ' ' +str(threshold)
+
+for i in range(0,train_dataset_df.shape[1]):
+    context.append(Categorical)
+for train_index,test_index in kf.split(train_dataset_df):
+    X_train,X_test=train_dataset_df.values[train_index],train_dataset_df.values[test_index]
+    X=numpy.nan_to_num(X_train)
+    X = X.astype(numpy.float32)
+    #X = preprocessing.normalize(X, norm='l2')
+    X_test = numpy.nan_to_num(X_test)
+    #X_test = preprocessing.normalize(X_test, norm='l2')
+    X = X.astype(numpy.float32)
+    X_test =X_test.astype(numpy.float32)
+    train_set.append(X)
+    test_set.append(X_test)
+    np.save('train', X)
+    np.save("test",X_test)
+    np.save("context",context)
+    P=subprocess.Popen(['./experiment.py train.npy test.npy context.npy '+opt_args.strip()],shell=True)
+    P.communicate()
+    P.wait();
+    P.terminate()
+print("process completed")
 #!/usr/bin/env python
 '''
 CREATED:2011-11-12 08:23:33 by Brian McFee <bmcfee@cs.ucsd.edu>
