aa4c1f876c4e52ecd505cd3dc8ffea36b3446d5e
Prajay Shetty <prajay.shetty24@gmail.com>
2020-07-31 18:11:46 +0530

    Added log files and results

--- scripts/plot.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-iris = sns.load_dataset("iris")
-
-g = sns.PairGrid(iris, y_vars=["sepal_length"], x_vars=["sepal_width","sepal_width"], height=2)
-g.map(plt.scatter, color=".3",edgecolor="white")
-g.set(ylim=(-1, 11), yticks=[0, 5, 10]);
-plt.show()
