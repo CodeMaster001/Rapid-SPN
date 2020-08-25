698e66be6ed59db0b0f8e297024de7bcddba1f73
Prajay Shetty <prajay.shetty24@gmail.com>
2020-08-25 12:29:12 +0530

    Updated files

--- scripts/cifar_10.py
+++ scripts/cifar_10.py
@@ -77,8 +77,7 @@ def clean_data(x):
         print(str(x))
 
  # experiment.py train.csv test.csv context.npy instance_slice epochs height prob leaves_size
-#train_dataset, y = fetch_openml('CIFAR_10', version=1, return_X_y=True)
-train_dataset = pd.read_csv('dataset/cifar-10.csv',delimiter=',')
+train_dataset, y = fetch_openml('CIFAR_10', version=1, return_X_y=True)
 train_dataset = pd.DataFrame(train_dataset)
 
 train_dataset=train_dataset.sample(n=int(sys.argv[1])).values
