f2cb4fc43ab6e6a41aed2a0c6090540edadb7bee
prajay shetty <prajay.shetty2016@gmail.com>
2024-09-14 13:31:01 +0530

    deleted unwanted dataset

---
+++ scripts/results/blood.50.log.cmd
@@ -0,0 +1,1 @@
+['experiment.py', 'train.npy', 'test.npy', 'context.npy', 'blood.50.log', '50', '8', '15', '0.4']
---
+++ scripts/results/bodyfat.10.log.cmd
@@ -0,0 +1,1 @@
+['experiment.py', 'train.npy', 'test.npy', 'context.npy', 'bodyfat.10.log', '20', '1', '15', '0.4']
