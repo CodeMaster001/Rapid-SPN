7bbed9ff1fc76ef796f682aa05cb91c6eb62a77a
prajay shetty <prajay.shetty2016@gmail.com>
2024-09-23 08:12:21 +0530

    Added balance scale results and code after refractoring

--- scripts/balance_scale.py
+++ scripts/balance_scale.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 import numpy
 import sys
@@ -31,18 +30,19 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import KFold
 from spn.structure.Base import *
+import time;
 import numpy as np, numpy.random
+numpy.random.seed(42)
+from utils import run_execution_float
+import multiprocessing
 import logging
 import subprocess
+#tf.logging.set_verbosity(tf.logging.INFO)
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
-
-
-
-numpy.random.seed(42)
-# experiment.py train.csv test.csv context.npy instance_slice epochs height prob leaves_size
+# experiment.py train.csv test.csv context.npy instance_slice epochs height prob leaves_sizefor instance_slice in [10,20,40,60]:
 train_dataset,labels= fetch_openml(name='balance-scale', version=1,return_X_y=True)
-train_dataset_df = pd.DataFrame(train_dataset)
+
 kf = KFold(n_splits=10,shuffle=True,random_state=42)
 theirs = list()
 ours = list()
@@ -54,36 +54,16 @@ counter = 0;
 context = list()
 
 #parameters
+
+min_instance_slice=20
 epochs=8000
 height=6
 prob=0.4
 leaves_size=14
 threshold =0.4
-instance_slice=20
 selector_array=[2,3,4]
-output_file_name='balance_scale.10.csv'
+output_file_name='balance.10.csv'
 np.save('selector',np.array(selector_array))
-for train_index,test_index in kf.split(train_dataset):
-    X_train,X_test = train_dataset.values[train_index],train_dataset.values[test_index]
-    context = []
-    for i in range(0,train_dataset_df.shape[1]):
-        context.append(Gaussian)
-    opt_args=str(output_file_name) + ' ' + str(instance_slice) +' ' +str(height) + ' '+str(leaves_size)+' '+str(threshold)
-    X_train,X_test=  train_test_split(train_dataset_df,test_size=0.4,random_state=42)
-    X=numpy.nan_to_num(X_train)
-    X = X.astype(numpy.float32)
-    X = preprocessing.normalize(X, norm='l2')
-    X_test = numpy.nan_to_num(X_test)
-    X_test = preprocessing.normalize(X_test, norm='l2')
-    X = X.astype(numpy.float32)
-    X_test =X_test.astype(numpy.float32)
-    train_set.append(X)
-    test_set.append(X_test)
-    np.save('train', X)
-    np.save("test",X_test)
-    np.save("context",context)
-    P=subprocess.Popen(['python3 experiment.py train.npy test.npy context.npy '+opt_args.strip()],shell=True)
-    P.communicate()
-    P.wait();
-    P.terminate()
-    print("process completed")
+
+for X_train_index,X_test_index  in kf.split(train_dataset):
+    run_execution_float(X_train=train_dataset.values[X_train_index],X_test=train_dataset.values[X_test_index],min_instance_slice=min_instance_slice,height=height,leaves_size=leaves_size,threshold=threshold,output_file_name=output_file_name)
