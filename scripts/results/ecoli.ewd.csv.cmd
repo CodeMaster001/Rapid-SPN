b3613d66f5911f3244718b4647afcb9941a2c0b4
prajay shetty <prajay.shetty2016@gmail.com>
2024-09-28 22:10:50 +0530

    added ecoli ewd dataset

---
+++ scripts/results/ecoli.ewd.csv.cmd
@@ -0,0 +1,3 @@
+['experiment.py', 'train.npy', 'test.npy', 'context.npy', 'ecoli.ewd.csv', '10', '4', '14', '0.4']
+['experiment.py', 'train.npy', 'test.npy', 'context.npy', 'ecoli.ewd.csv', '10', '4', '14', '0.4']
+['experiment.py', 'train.npy', 'test.npy', 'context.npy', 'ecoli.ewd.csv', '10', '4', '14', '0.4']
