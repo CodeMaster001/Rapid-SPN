2d643cfbe2bfabcd0d9a11ef291b2f18dc8c3016
prajay shetty <prajay.shetty2016@gmail.com>
2024-09-28 21:33:20 +0530

    added scripts

---
+++ scripts/car_ewd.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import numpy
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from sklearn import preprocessing
+from sklearn.datasets import load_svmlight_file
+from spatialtree import *;
+from sklearn.model_selection import train_test_split
+from libsvm.svmutil import *
+from spn.structure.Base import Context
+from spn.io.Graphics import plot_spn
+from spn.algorithms.Sampling import sample_instances
+from spn.structure.leaves.parametric.Parametric import Categorical, Gaussian
+from spn.algorithms.Inference import log_likelihood
+from spn.algorithms.splitting.RDC import get_split_cols_RDC_py, get_split_rows_RDC_py
+from sklearn.datasets import load_iris,load_digits,fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
+from spn.algorithms.LearningWrappers import learn_parametric
+from spn.structure.Base import Product, Sum, assign_ids, rebuild_scopes_bottom_up
+from sklearn.metrics import accuracy_score
+from numpy.random.mtrand import RandomState
+from spn.algorithms.LearningWrappers import learn_parametric, learn_classifier
+from spn.algorithms.TransformStructure import Prune,Compress,SPN_Reshape
+import urllib
+from sklearn.preprocessing import LabelEncoder
+from sklearn.datasets import fetch_openml
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import KFold
+from spn.structure.Base import *
+import time;
+import numpy as np, numpy.random
+numpy.random.seed(42)
+import multiprocessing
+import logging
+from utils import run_execution_character
+import subprocess
+#tf.logging.set_verbosity(tf.logging.INFO)
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+
+
+# experiment.py train.csv test.csv context.npy instance_slice epochs height prob leaves_size
+
+train_dataset_df,labels= fetch_openml(name='car', version=1,return_X_y=True)
+le = preprocessing.LabelEncoder()
+train_dataset_df = train_dataset_df[train_dataset_df.columns].apply(le.fit_transform)
+train_dataset_df = train_dataset_df.sample(48)
+kf = KFold(n_splits=10,shuffle=True)
+theirs = list()
+ours = list()
+ours_time_list = list()
+theirs_time_list = list();
+train_set = list()
+test_set = list();
+counter = 0;
+context = list()
+
+#parameters
+output_file_name='car.ewd.csv'
+min_instance_slice=40
+epochs=8000
+height=2
+prob=0.4
+leaves_size=16
+threshold =0.4
+
+X_train,X_test  = train_test_split(train_dataset_df)
+run_execution_character(X_train=X_train,X_test=X_test,min_instance_slice=min_instance_slice,height=height,leaves_size=leaves_size,threshold=threshold,output_file_name=output_file_name)
