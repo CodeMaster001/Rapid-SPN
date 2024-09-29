0f5e53b9e2c8c23c706486bcc3a97f6b4e314970
prajay shetty <prajay.shetty2016@gmail.com>
2024-09-29 09:06:25 +0530

    deleted files

--- scripts/results/ecoli.ewd.csv.cmd
+++
@@ -1,3 +0,0 @@
-['experiment.py', 'train.npy', 'test.npy', 'context.npy', 'ecoli.ewd.csv', '10', '4', '14', '0.4']
-['experiment.py', 'train.npy', 'test.npy', 'context.npy', 'ecoli.ewd.csv', '10', '4', '14', '0.4']
-['experiment.py', 'train.npy', 'test.npy', 'context.npy', 'ecoli.ewd.csv', '10', '4', '14', '0.4']
