041c4f406d7534168e2bf04cc0ec7ce25ffdc5c5
prajay shetty <prajay.shetty2016@gmail.com>
2024-09-30 16:51:19 +0530

    regneeratd results

--- scripts/results/car.10.csv.cmd
+++ scripts/results/car.10.csv.cmd
@@ -8,6 +8,3 @@
 ['experiment.py', 'train.npy', 'test.npy', 'context.npy', 'car.10.csv', '40', '2', '16', '0.4']
 ['experiment.py', 'train.npy', 'test.npy', 'context.npy', 'car.10.csv', '40', '2', '16', '0.4']
 ['experiment.py', 'train.npy', 'test.npy', 'context.npy', 'car.10.csv', '40', '2', '16', '0.4']
-
-['experiment.py', 'train.npy', 'test.npy', 'context.npy', 'car.10.csv', '40', '2', '16', '0.4']
-['experiment.py', 'train.npy', 'test.npy', 'context.npy', 'car.10.csv', '40', '2', '16', '0.4']
